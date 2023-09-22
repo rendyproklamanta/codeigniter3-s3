3947d576444376eb1861425ebcfad9fa9f296198
rendyproklamanta <rendyproklamanta@gmail.com>
2023-09-22 14:48:33 +0700

    initial

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -22,13 +22,13 @@ class Upload extends CI_Controller
    function doUpload()
    {
       $uploadedFileName = 'userfile'; // file name in upload form
-      $type = 'image'; // set directory upload
+      $type = 'image'; // set upload type : image,doc,pdf,excel
       $directory = 'avatar'; // set directory upload
       $s3Upload = json_encode(s3Upload($type, $uploadedFileName, $directory));
       $res = json_decode($s3Upload); // convert to object
 
       if ($res->success) {
-         // print_r($res->data); // will return URL -> than save to database
+         // print_r($res->data); // will return URL -> and then save the URL to database
          print_r($res);
       } else {
          print_r($res->message);
